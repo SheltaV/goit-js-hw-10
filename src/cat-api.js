0177f1aa6b03ac4989c6826e2148a9db5b13900f
SheltaV <bluegiraffezaoz@gmail.com>
2023-07-24 23:10:21 +0300

    update-hw-10

---
+++ src/cat-api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = 'live_g4xdVT3JQPdOSwPJnekRMueaO56Do7WTVacvOm1COJN509fGsFIZ2jtv91CHRfRu';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+
+export function fetchBreeds() {
+    return axios.get('/breeds')
+    .then(resp => {
+        if (resp.status !== 200) {
+        throw new Error(resp.statusText)
+        }
+        return resp.data;
+    })
+}
+
+export function fetchCatByBreed(breedId) {
+    return axios.get(`/images/search?breed_ids=${breedId}`)
+    .then(resp => {
+        if (resp.status !== 200) {
+        throw new Error(resp.statusText)
+        }
+        return resp.data;
+    })
+}
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
+
+const selector = document.querySelector('.breed-select');
+const info = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error')
+
+fetchBreeds()
+    .then(data => {
+    selector.innerHTML = data.map(({ id, name }) =>
+        `<option value="${id}">${name}</option>`).join('')
+    })
+    .catch(err => console.log(err))
+    .finally(() => loader.setAttribute('hidden', true))
+
+selector.addEventListener('change', changeCat);
+
+loader.setAttribute('hidden', true)
+error.setAttribute('hidden', true)
+
+function changeCat(event) {
+    info.setAttribute('hidden', true)
+    loader.removeAttribute('hidden')
+    fetchCatByBreed(event.target.value)
+        .then(data => {
+            info.removeAttribute('hidden')
+            selector.removeAttribute('hidden')
+        let catInformation = data[0].breeds[0];
+        const { name, description, temperament} = catInformation;
+        info.innerHTML = data.map(({url}) => `<img src="${url}" alt="${name}" width="400" />`).join('')
+        info.insertAdjacentHTML('beforeend', 
+    `<h2>${name}</h2>
+    <h3>${description}</h3>
+    <p>${temperament}</p>`)
+    })
+    .catch(() => Notiflix.Notify.failure(error.textContent))
+    .finally(() => loader.setAttribute('hidden', true))
+}
+
