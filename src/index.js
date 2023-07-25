e469c355a3d472b1b3a7f1f19058dcfcf6104532
SheltaV <bluegiraffezaoz@gmail.com>
2023-07-25 11:26:46 +0300

    update-js-hw-10

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import Notiflix from 'notiflix';
 const selector = document.querySelector('.breed-select');
 const info = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error')
+const error = document.querySelector('.error');
+
+// loader.setAttribute('hidden', true)
+error.setAttribute('hidden', true)
+selector.setAttribute('hidden', true)
+
 
 fetchBreeds()
     .then(data => {
-    selector.innerHTML = data.map(({ id, name }) =>
+        selector.removeAttribute('hidden')
+    return selector.innerHTML = data.map(({ id, name }) =>
         `<option value="${id}">${name}</option>`).join('')
     })
     .catch(err => console.log(err))
@@ -16,13 +22,10 @@ fetchBreeds()
 
 selector.addEventListener('change', changeCat);
 
-loader.setAttribute('hidden', true)
-error.setAttribute('hidden', true)
 
 function changeCat(event) {
     info.setAttribute('hidden', true)
     loader.removeAttribute('hidden')
-    selector.setAttribute('hidden', true)
     fetchCatByBreed(event.target.value)
         .then(data => {
             info.removeAttribute('hidden')
@@ -35,7 +38,9 @@ function changeCat(event) {
     <h3>${description}</h3>
     <p>${temperament}</p>`)
     })
-    .catch(() => Notiflix.Notify.failure(error.textContent))
+        .catch(() => { info.setAttribute('hidden', true)
+            return Notiflix.Notify.failure(error.textContent)
+        })
     .finally(() => loader.setAttribute('hidden', true))
 }
 
